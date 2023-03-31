bc79068cd3a526af926306aec5b3167d53d20b7a
Patrick Pelletier <patrick.pelletier@unity3d.com>
2023-03-31 14:45:44 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,151 @@
+import * as THREE from 'three';
+import { toneMapping } from 'three/nodes';
+
+import WebGPU from 'three/addons/capabilities/WebGPU.js';
+import WebGPURenderer from 'three/addons/renderers/webgpu/WebGPURenderer.js';
+
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { FlyControls } from 'three/addons/controls/FlyControls.js';
+
+import { Sky } from 'three/addons/objects/Sky.js';
+
+let useOrbitControls = true;
+let useWebGPU = WebGPU.isAvailable();
+
+let renderer;
+
+if (useWebGPU) {
+    renderer = new WebGPURenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    document.body.appendChild(renderer.domElement);
+}
+else {
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 0.2;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const cameraLight = new THREE.DirectionalLight(0xffffff, 5);
+
+let uniforms;
+if (!useWebGPU) {
+    let sky = new Sky();
+    sky.scale.setScalar(450000);
+    scene.add(sky);
+
+    uniforms = sky.material.uniforms;
+    uniforms['turbidity'].value = 10;
+    uniforms['rayleigh'].value = 3;
+    uniforms['mieCoefficient'].value = 0.005;
+    uniforms['mieDirectionalG'].value = 0.7;
+}
+
+scene.add(cameraLight)
+scene.background = new THREE.Color(0x606060);
+
+const loader = new GLTFLoader();
+
+let variants;
+let index = 0;
+let parser;
+let variantsExtension;
+
+loader.load('ContainerModelExported.glb', function (gltf) {
+    scene.add(gltf.scene);
+
+    const box = new THREE.Box3().setFromObject(gltf.scene);
+    const center = box.getCenter(new THREE.Vector3());
+
+    gltf.scene.position.set(-center.x, -center.y, -center.z);
+
+    parser = gltf.parser;
+    variantsExtension = gltf.userData.gltfExtensions['KHR_materials_variants'];
+    variants = variantsExtension.variants.map((variant) => variant.name);
+
+    if (variants.length > 0)
+        selectVariant(scene, parser, variantsExtension, variants[0]);
+
+}, undefined,
+    function (error) {
+        console.error(error);
+    });
+
+let controls;
+if (useOrbitControls) {
+    controls = new OrbitControls(camera, renderer.domElement);
+    camera.position.set(0, 2, 10);
+    controls.update();
+}
+else {
+    controls = new FlyControls(camera, renderer.domElement);
+    controls.dragToLook = true;
+    controls.rollSpeed = 0.05;
+}
+
+window.setInterval(function () {
+    selectVariant(scene, parser, variantsExtension, variants[index++ % variants.length]);
+}, 1000);
+
+if (!useWebGPU)
+    animate();
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    if (useOrbitControls)
+        controls.update();
+    else
+        controls.update(0.16);
+
+    let vec = new THREE.Vector3;
+    camera.getWorldDirection(vec);
+    cameraLight.position.x = -vec.x;
+    cameraLight.position.y = -vec.y;
+    cameraLight.position.z = -vec.z;
+
+    if (!useWebGPU) {
+        const phi = THREE.MathUtils.degToRad(90 - (new Date().getTime() / 1000) % 180);
+        const theta = THREE.MathUtils.degToRad(180);
+        let sun = new THREE.Vector3();
+        sun.setFromSphericalCoords(1, phi, theta);
+        uniforms['sunPosition'].value.copy(sun);
+    }
+
+    renderer.render(scene, camera);
+}
+
+function selectVariant(scene, parser, extension, variantName) {
+    const variantIndex = extension.variants.findIndex((v) => v.name.includes(variantName));
+    scene.traverse(
+        async (object) => {
+            if (!object.isMesh || !object.userData.gltfExtensions)
+                return;
+
+            const meshVariantDef = object.userData.gltfExtensions['KHR_materials_variants'];
+
+            if (!meshVariantDef)
+                return;
+
+            if (!object.userData.originalMaterial) {
+                object.userData.originalMaterial = object.material;
+            }
+
+            const mapping = meshVariantDef.mappings.find((mapping) => mapping.variants.includes(variantIndex));
+
+            if (mapping) {
+                object.material = await parser.getDependency('material', mapping.material);
+                parser.assignFinalMaterial(object);
+            }
+            else {
+                object.material = object.userData.originalMaterial;
+            }
+        });
+}
