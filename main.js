39b50968516fdda61908b39495104eb7dd8964c8
Patrick Pelletier <patrick.pelletier@unity3d.com>
2023-04-05 10:02:50 -0400

    improved color balance

--- main.js
+++ main.js
@@ -7,19 +7,18 @@ import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 import { GroundProjectedSkybox } from "three/addons/objects/GroundProjectedSkybox.js";
 
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
+import { SSRPass } from 'three/addons/postprocessing/SSRPass.js';
 import { TAARenderPass } from 'three/addons/postprocessing/TAARenderPass.js';
 import { CopyShader } from 'three/addons/shaders/CopyShader.js';
-
-const param = { TAAEnabled: '1', TAASampleLevel: 5 };
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 
 let renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setAnimationLoop(render);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 4;
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = 5;
 renderer.outputColorSpace = THREE.SRGBColorSpace;
 renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
@@ -29,16 +28,37 @@ let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(23, window.innerWidth / window.innerHeight, 0.25, 200);
 camera.position.set(6, 2, 10);
 
-// taa
 let composer = new EffectComposer(renderer, new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, { type: THREE.HalfFloatType }));
 
+/*
+let ssrPass = new SSRPass({
+    renderer,
+    scene,
+    camera,
+    width: innerWidth,
+    height: innerHeight,
+    groundReflector: null,
+    selects: null
+});
+ssrPass.opacity = 1;
+
+composer.addPass(ssrPass);
+//composer.addPass(new ShaderPass(GammaCorrectionShader));
+*/
+
+// taa
 let taaRenderPass = new TAARenderPass(scene, camera);
-taaRenderPass.unbiased = false;
+taaRenderPass.unbiased = false; // we're using half floats anyways
+taaRenderPass.enabled = true;
+taaRenderPass.sampleLevel = 3;
+taaRenderPass.accumulate = false;
 composer.addPass(taaRenderPass);
 
-let renderPass = new RenderPass(scene, camera);
-renderPass.enabled = false;
-composer.addPass(renderPass);
+const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
+bloomPass.threshold = 0.85;
+bloomPass.strength = 1;
+bloomPass.radius = 0.25;
+composer.addPass(bloomPass);
 
 let copyPass = new ShaderPass(CopyShader);
 composer.addPass(copyPass);
@@ -55,7 +75,7 @@ window.addEventListener('resize', () => {
 });
 
 // shadow plane
-const geometry = new THREE.PlaneGeometry(20, 20);
+const geometry = new THREE.PlaneGeometry(100, 100);
 geometry.rotateX(-Math.PI / 2);
 
 const material = new THREE.ShadowMaterial();
@@ -71,32 +91,35 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 0, 0);
 controls.update();
 
+// load low res dome and reflection map
 new RGBELoader().setPath('assets/').load('construction_2k.hdr', function (texture) {
     texture.mapping = THREE.EquirectangularReflectionMapping;
 
     scene.background = texture;
     scene.environment = texture;
 
-    // create low res
+    // create low res dome
     let ground = new GroundProjectedSkybox(texture);
-    ground.scale.setScalar(100);
-    ground.height = 5;
+    ground.scale.setScalar(35);
+    ground.height = 3;
     scene.add(ground);
 
     render();
 
+    // load hires dome
     new RGBELoader().setPath('assets/').loadAsync('construction_8k.hdr', progress => {
         render();
     }).then(texture => {
         texture.mapping = THREE.EquirectangularReflectionMapping;
         scene.background = texture;
 
-        // remove low res
+        // remove low res dome
         scene.remove(ground);
 
+        // add 'gumdrop' dome
         ground = new GroundProjectedSkybox(texture);
-        ground.scale.setScalar(100);
-        ground.height = 5;
+        ground.scale.setScalar(35);
+        ground.height = 3;
         scene.add(ground);
 
         render();
@@ -104,19 +127,33 @@ new RGBELoader().setPath('assets/').load('construction_2k.hdr', function (textur
 
     // yard
     new GLTFLoader().setPath('assets/').loadAsync('construction.glb').then(gltf => {
+        /* having a hard time tuning the shadow casting light for a larg-ish scene, we'll have the product only cast a shadow for now
+        // enable shadows
+        gltf.scene.traverse(child => {
+            if (child.isMesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+            }
+        });
+        */
         scene.add(gltf.scene);
+
+        // hide unwanted meshes
         gltf.scene.getObjectByName('CircularFloor').visible = false;
 
-        // shadow only
+        // disable sun and use shadow only
         let sun = gltf.scene.getObjectByName('Sun_Light');
         sun.castShadow = true;
-        sun.intensity = 0.01;
+        sun.intensity = 1.0;
 
-        sun.shadow.mapSize.width = 2048;
-        sun.shadow.mapSize.height = 2048;
+        sun.shadow.mapSize.width = 1024;
+        sun.shadow.mapSize.height = 1024;
+        sun.shadow.camera.near = 0.5; //default
+        sun.shadow.camera.far = 500; //default
 
+        // give a better orientation - is the glb coordinate system for lights correct?
         sun.target = plane;
-        sun.position.set(1, 0.5, -0.25);
+        sun.position.set(50, 20, 0);
 
         render();
     });
@@ -125,17 +162,18 @@ new RGBELoader().setPath('assets/').load('construction_2k.hdr', function (textur
     new GLTFLoader().setPath('assets/').loadAsync('skidLoader.glb').then(gltf => {
         gltf.scene.rotation.set(0, THREE.MathUtils.degToRad(90), 0);
 
+        // look at center of the model
         let box = new THREE.Box3().setFromObject(gltf.scene);
         let sphere = new THREE.Sphere();
         box.getBoundingSphere(sphere);
         controls.target.set(sphere.center.x, sphere.center.y, sphere.center.z);
         controls.update();
 
+        // enable shadows
         gltf.scene.traverse(child => {
             if (child.isMesh) {
                 child.castShadow = true;
                 child.receiveShadow = true;
-                child.material.envMapIntensity = 1.5;
                 // reflection debugging only
                 /*child.material.metalness = 0.5;
                 child.material.roughness = 0;*/
@@ -144,6 +182,13 @@ new RGBELoader().setPath('assets/').load('construction_2k.hdr', function (textur
 
         scene.add(gltf.scene);
 
+        // use proper fork color
+        gltf.scene.getObjectByName('Forks_Frame-1').traverse(child => {
+            if (child.isMesh)
+                child.material.color.set(0x000000);
+        });
+
+        // hide unwanted meshes and lights
         gltf.scene.getObjectByName('Spot_Light_Left').visible = false;
         gltf.scene.getObjectByName('Spot_Light_Right').visible = false;
 
@@ -165,10 +210,5 @@ new RGBELoader().setPath('assets/').load('construction_2k.hdr', function (textur
 });
 
 function render() {
-    taaRenderPass.enabled = (param.TAAEnabled === '1');
-    renderPass.enabled = (param.TAAEnabled !== '1');
-    taaRenderPass.sampleLevel = param.TAASampleLevel;
-    taaRenderPass.accumulate = false;
-
     composer.render();
 }
